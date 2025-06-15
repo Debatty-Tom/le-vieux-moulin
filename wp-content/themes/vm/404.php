da2ca27d3ed71cfeddfb6067a36ef2091084de7e
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-06-15 15:07:26 +0200

    add 404 page

---
+++ wp-content/themes/vm/404.php
@@ -0,0 +1,7 @@
+<?php get_header() ?>
+<section class="notfound">
+    <h2 class="notfound__title">404 - Page non trouvée</h2>
+    <p class="notfound__text">La page que vous avez recherché n'existe pas.</p>
+    <a class="notfound__link button" href="<?= home_url() ?>">Retour à la page d'accueil</a>
+</section>
+<?php get_footer() ?>
